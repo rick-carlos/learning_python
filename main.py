12d18533088eecf3f8c39bb67bc32ba126319d0f
rick-kinzey <chcavil@gmail.com>
2025-02-07 19:25:40 -0300

    Armazena o nome do usuário e o responde

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print('olá mundo')
+from scripts.your_name import Name
+
+# se o script estiver em outra pasta no mesmo diretório, cite a pasta e o arquivo separado por ( . )
+
+Name()
+
---
+++ scripts/your_name.py
@@ -0,0 +1,5 @@
+
+class Name:   #Classes por convenção são definidas com inicial maiúscula.
+    ola = input("Olá, qual é o seu nome? ")
+    print(f'Seja bem vindo {ola}')
+
